5839c4f9eea7bb2d70ecf6408179f4c91b9821a1
dodong471520 <1012131075@qq.com>
2018-04-04 00:48:31 +0800

    ds

--- install-gdb.sh
+++ install-gdb.sh
@@ -3,9 +3,9 @@
 #install textinfo
 
 source  ./install-define.sh
-ln -fs $GIT_PATH/cyrus-and/gdb-dashboard/.gdbinit ~/.gdbinit
+ln -fs $MY_GIT/gdb-dashboard/.gdbinit ~/.gdbinit
 ln -fs $DOT_PATH/.gdbinit.d ~/.gdbinit.d
 
 
-ghq get https://github.com/cyrus-and/gdb-dashboard
+ghq get https://github.com/dodong471520/gdb-dashboard
 
