6c2932e225db3c2f4d409e723516ad2ca12a8a87
dodong471520 <1012131075@qq.com>
2018-04-16 00:53:55 +0800

    ds

--- install-sub/gr-get.sh
+++ install-sub/gr-get.sh
@@ -7,7 +7,7 @@ echo $1
 TO_NAME=`basename $1`
 
 
-git clone --recurse-submodules -j8 http://github.com/$1 $MY_GIT/$TO_NAME 
+git clone --recurse-submodules -j8 http://github.com/$1 $MY_GIT/$TO_NAME
 
-gr tag work add $MY_GIT/$TO_NAME
+gr +@work $MY_GIT/$TO_NAME
 
