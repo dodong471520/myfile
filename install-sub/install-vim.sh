e01891da452e55fd1337af3cae1a24a85d8ba042
dodong471520 <1012131075@qq.com>
2018-04-18 21:17:45 +0800

    ds

--- install-sub/install-vim.sh
+++ install-sub/install-vim.sh
@@ -5,7 +5,8 @@ source ./install-define.sh
 dnf list installed vim-X11||dnf install vim-X11
 
 ln -fns $MY_GIT/.vim ~/.vim
-ln -fs $MY_GIT/space-vim/init.vim ~/.vimrc
+ln -fs $DOT_PATH/.vimrc ~/.vimrc
+ln -fs $MY_GIT/space-vim/init.vim ~/.init.vim
 ln -fns $MY_GIT/space-vim ~/.space-vim
 ln -fs $MY_GIT/space-vim/init.spacevim ~/.spacevim
 ln -fs $DOT_PATH/.NERDTreeBookmarks ~/.NERDTreeBookmarks
